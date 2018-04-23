962cfda52f6b27d152493a3ac24ae2f9e660f050
dangjustintime <dangjustintime@gmail.com>
2018-04-23 16:10:46 -0400

    made Mem_ref struct

--- proj3_dang.c
+++ proj3_dang.c
@@ -1,8 +1,21 @@
 #include <stdlib.h>
 
-int main() {
+struct Mem_Ref {
+  int address;
+  int tag;
+  int index;
+  int offset;
+  int result;
+};
 
-  
+int main() {
+  struct Mem_Ref mem_ref[100];
+  char line[30];
+  int block_size;
+  int num_sets;
+  int set_associativity;
+
+   
 
   return 0;
 }
